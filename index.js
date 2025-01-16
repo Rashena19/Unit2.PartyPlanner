7f08278af1e82e400cf3cbd81056fc57a54d28bc
Rashena Hanif <rashenahanif@MacBookPro.lan>
2025-01-16 01:04:41 -0500

    Party Planner, need to fix delete function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const COHORT = "2409-GHP-ET-WEB-PT";
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
+const BASE_URL = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/' + COHORT;
 
 // === State ===
 
@@ -7,51 +7,36 @@ const state = {
   events: [],
 };
 
+const form = document.getElementById('partyPlanner');
+const eventlist = document.getElementById('partyList');
+
 /** Updates state with events from API (pulling events from API) */
 async function getEvent() {
   try {
-    const promise = await fetch(API_URL);
-    const response = await promise.json();
-    if (!response.success) {
-      throw response.error;
+    const response = await fetch(`${BASE_URL}/events`);
+    const json = await response.json();
+    if (!json.success) {
+      throw json.error;
     }
-    state.artists = response.data;
+    state.events = json.data;
   } catch (error) {
-    alert("Unable to load Events");
+    console.error("Unable to load Events");
   }
 }
 
-/** Asks the API to create a new artist based on the given `artist` */
-async function addArtist(artist) {
+/** Asks the API to create a new artist based on the given `Events` */
+async function addEvent(newEvent) {
   try {
-    const response = await fetch(API_URL, {
+    const response = await fetch(`${BASE_URL}/events`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(artist),
+      body: JSON.stringify(newEvent),
     });
     if (!response.ok) {
       throw new Error(
-        "Unable to add artist due to Http error: " + response.status
-      );
-    }
-  } catch (error) {
-    alert(error.message);
-  }
-}
-/** Asks the API to delete the given artist */
-async function deleteArtist(artist) {
-  try {
-    const response = await fetch(API_URL + "/" + artist.id, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (!response.ok) {
-      throw new Error(
-        "Unable to delete artist due to Http error: " + response.status
+        "Unable to add Event due to Http error: " + response.status
       );
     }
     render();
@@ -59,69 +44,93 @@ async function deleteArtist(artist) {
     alert(error.message);
   }
 }
+/** Asks the API to delete the given event */
+async function deleteEvent(id) {
+  try {
+    const response = await fetch(`${BASE_URL}/events/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(
+        "Unable to delete event due to Http error: " + response.status
+      );
+    }
+    render();
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-// === Render ===
 
-/** Renders artists from state */
-function renderArtists() {
-  const artistList = document.querySelector("#artists");
+// Event Listener
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  try {
 
-  if (!state.artists.length) {
-    artistList.innerHTML = "<li>No artists.</li>";
+    const eventDate = new Date(form.date.value).toISOString();
+
+    const newParty = {
+      name: form.eventName.value,
+      description: form.discription.value,
+      date: eventDate,
+      location: form.location.value,
+    };
+
+    await addEvent(newParty);
+
+
+    form.reset();
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+/** Renders event from state */
+function renderEvent() {
+  const eventList = document.querySelector("#events");
+
+  if (!state.events.length) {
+    eventList.innerHTML = "<li>No events.</li>";
     return;
   }
 
-  const artistCards = state.artists.map((artist) => {
+  const eventCards = state.events.map((event) => {
     const card = document.createElement("li");
+
     //H1 for Artist Name
     const h1 = document.createElement("h1");
-    h1.textContent = artist.name;
+    h1.textContent = event.name;
 
-    //H2 for Artist Description
+    //H2 for event Description
     const h2 = document.createElement("h2");
-    h2.textContent = artist.description;
+    h2.textContent = event.description;
 
-    //Image of Artist
-    const image = document.createElement("img");
-    image.src = artist.imageUrl; //set the img src to be the imageUrl from the artist object
-    image.style.width = "50%";
-    image.style.height = "50%";
+    //h3 Event Date 
+    const eventDate = new Date(event.date).toLocaleString();
 
-    //Button to Delete the Artist
+
+    //Button to Delete the Event
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
     deleteButton.style.display = "block";
     deleteButton.addEventListener("click", async () => {
-      await deleteArtist(artist);
-    });
-
-    card.append(h1, h2, image, deleteButton);
-    return card;
+      console.log("Delete button clicked!");
+      await deleteEvent(event);
+    })
+    card.append(h1, h2, eventDate, deleteButton)
+    return card
   });
+  eventList.replaceChildren(...eventCards);
 
-  artistList.replaceChildren(...artistCards);
 }
 /** Syncs state with the API and rerender */
 async function render() {
-  await getArtists();
-  renderArtists();
+  await getEvent();
+  renderEvent();
 }
 
-// === Script ===
-
 render();
-
-// Add listener to form
-const form = document.getElementById("addArtist");
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-
-  const artist = {
-    name: form.artistName.value,
-    description: form.description.value,
-    imageUrl: form.imageUrl.value,
-  };
-
-  await addArtist(artist);
-  render();
-});
